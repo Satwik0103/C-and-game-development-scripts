29e531e7295dd100f57eb71ecae41ea0231beab7
Satwik Saxena <53349180+Satwik0103@users.noreply.github.com>
2020-07-17 23:33:27 +0530

    Add files via upload

---
+++ Pipes.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipes : MonoBehaviour
+{
+    public float moveSpeed=0.1f;
+    void Update()
+    {
+        transform.position -= new Vector3(moveSpeed, 0, 0);
+    }
+}
---
+++ SpawnManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    public GameObject pipes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnPipe", 2, 2);
+    }
+
+    void SpawnPipe()
+    {
+        Instantiate(pipes, new Vector3(pipes.transform.position.x, pipes.transform.position.y + Random.Range(2, 2), pipes.transform.position.z), Quaternion.identity);
+
+    }
+}
